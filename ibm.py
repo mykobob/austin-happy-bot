2f2b212e3e81f7abb4388b68bf569a0f66ff8833
mykobob <michael.eagle@utexas.edu>
2017-10-21 19:18:30 -0500

    debug

--- ibm.py
+++ ibm.py
@@ -4,6 +4,10 @@ import watson_developer_cloud.natural_language_understanding.features.v1 as \
     Features
 
 def call_ibm_watson(text):
+    username = os.environ['BLUEMIX_USERNAME']
+    password = os.environ['BLUEMIX_PASSWORD']
+    print('user, pass')
+    print(username, password)
     natural_language_understanding = NaturalLanguageUnderstandingV1(
         version='2017-02-27',
         username=os.environ['BLUEMIX_USERNAME'],
